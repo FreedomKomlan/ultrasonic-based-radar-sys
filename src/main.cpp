c0978a39accb83ceff57c6e013ff8b7c03e542dc
gds-tech <90794220+FreedomKomlan@users.noreply.github.com>
2026-02-13 16:43:44 +0100

    Commit on 2026-02-13 16:43: Testing ultrasonic sensor

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,16 @@ Ultrasonic ultrasonic;
 void setup() {
   // put your setup code here, to run once:
   ultrasonic.init(TRIGGER_PIN, ECHO_PIN);
+  pinMode(LED_PIN, OUTPUT);
   // Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
   Serial.begin(115200);
+  digitalWrite(LED_PIN, HIGH);
 } 
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  // digitalWrite(LED_PIN, HIGH);
+  // delay(100);
   float distance = ultrasonic.measureDistance();
   Serial.println(distance);
 }
