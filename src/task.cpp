ab0de8df1a909155c2e9f0a23d74dc5350462d3e
gds-tech <90794220+FreedomKomlan@users.noreply.github.com>
2026-03-13 12:36:44 +0000

    Commit on 2026-03-13 12:36: Scan area task added and integrated with servo motor control. Testing Angle display and servo movement

--- src/task.cpp
+++ src/task.cpp
@@ -41,19 +41,23 @@ void sendText_task(void *pvParameters) {
 
 void scanArea_task(void *pvParameters) {
     ServoMotor *servoMotor = static_cast<ServoMotor *>(pvParameters);
+    Ultrasonic *ultrasonic = static_cast<Ultrasonic *>(pvParameters);
+    float distance(0.0f);
     // ServoMotor servoMotor;
     while (true) {
         for (int angle = ZONE_ANGLE_THRESHOLD_MIN_PRESET_DEG; angle <= ZONE_ANGLE_THRESHOLD_MAX_PRESET_DEG; angle += 1) {
             servoMotor->setAngle(angle);
             int actualAngle = servoMotor->getAngle();
-            String text_to_send_screen = "Angle: " + String(actualAngle) + " deg";
+            distance = ultrasonic->measureDistance();
+            String text_to_send_screen = "Angle: " + String(actualAngle) + " deg" + "\n Distance objet: " + String(distance) + " " + UNIT_DISTANCE;
             sendTextToDisplay(text_to_send_screen.c_str());
             vTaskDelay(200 / portTICK_PERIOD_MS); // Delay to read the angle on the display before moving to the next one
         }
         for (int angle = ZONE_ANGLE_THRESHOLD_MAX_PRESET_DEG; angle >= ZONE_ANGLE_THRESHOLD_MIN_PRESET_DEG; angle -= 1) {
             servoMotor->setAngle(angle);
             int actualAngle = servoMotor->getAngle();
-            String text_to_send_screen = "Angle: " + String(actualAngle) + " deg";
+            distance = ultrasonic->measureDistance();
+            String text_to_send_screen = "Angle: " + String(actualAngle) + " deg" + "\n Distance objet: " + String(distance) + " " + UNIT_DISTANCE;
             sendTextToDisplay(text_to_send_screen.c_str());
             vTaskDelay(200 / portTICK_PERIOD_MS); // Delay to read the angle on the display before moving to the next one
         }
